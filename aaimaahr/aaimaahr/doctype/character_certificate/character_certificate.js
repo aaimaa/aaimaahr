d1faa1d1237b185547a14f15f9f34e7e01eb3e6f
root <root@ip-172-31-12-250.ap-south-1.compute.internal>
2017-10-24 21:17:22 +0000

    Testing another branch

---
+++ aaimaahr/aaimaahr/doctype/character_certificate/character_certificate.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Aaimaa Web Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Character Certificate', {
+	refresh: function(frm) {
+
+	}
+});
