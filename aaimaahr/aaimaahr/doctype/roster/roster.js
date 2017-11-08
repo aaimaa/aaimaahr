969e47bff68a85bb040dd8e068ff78688935adad
root <root@ip-172-31-12-250.ap-south-1.compute.internal>
2017-11-08 11:29:51 +0000

    Added: Shift Planning & Roster

---
+++ aaimaahr/aaimaahr/doctype/roster/roster.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2017, Aaimaa Web Solutions and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Roster', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ aaimaahr/aaimaahr/doctype/shift_details/test_shift_details.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017, Aaimaa Web Solutions and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+import frappe
+import unittest
+
+class TestShiftDetails(unittest.TestCase):
+	pass
